0f582ebf2aaefbae2c1ed51cdca9d16e50bc3073
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2024-02-29 17:12:08 +0700

    задание и вывод массива в program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Методы вывода массива на экран
+
+void PrintArray(string[] array)
+{
+    System.Console.Write("[" + array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write("," + " " + array[i]);
+    }
+    System.Console.Write("]");
+    System.Console.WriteLine();
+}
+
+// Задаём массив из 4 строк
+string[] StrArray = new string[4] {"Hello", "2", "world", ":-)"};
+
+PrintArray(StrArray);
