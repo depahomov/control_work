fbd682a4ccbee2254ee4602b59373819f94c2a9f
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2024-03-03 13:51:02 +0700

    new version

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿// Метод вывода массива на экран
 void PrintArray(string[] array)
 {
+
     System.Console.Write("[" + array[0]);
-    for (int i = 1; i < array.Length; i++)
+    if (array.Length > 0)
     {
-        System.Console.Write("," + " " + array[i]);
+        for (int i = 1; i < array.Length; i++)
+        {
+            System.Console.Write("," + " " + array[i]);
+        }
     }
     System.Console.Write("]");
+    
+    // else
+    // {
+    //     System.Console.Write("[]");
+    // }
 }
 
 // Метод подсчёта количества элементов (строк) в исходном массиве с длиной не более 3 символов 
@@ -27,10 +36,10 @@ int CountingNum(string[] array)
 string[] MakeArray(string[] array)
 {
     int count = CountingNum(array);
-    string[] NewArray = new string[count];
-    int number = 0;
     if (count > 0)
     {
+        string[] NewArray = new string[count];
+        int number = 0;
         for (int i = 0; i < array.Length; i++)
         {
             if (array[i].Length < 4)
@@ -40,13 +49,18 @@ string[] MakeArray(string[] array)
             }
         }
     }
+    else 
+    {
+        string[] NewArray = new string[1];
+        NewArray[0] = "";
+    }
     return NewArray;
 }
 
 //------------------------------------------------------------------------
 //
 // Задаём массив из строк
-string[] StrArray = new string[6] {"Get", "1234", "Hello", "2", "world", ":-)"};
+string[] StrArray = new string[4] {"1", "1234", "Hello", "world"};
 // Формируем новый массив из элементов исходного массива, 
 // длина которых не больше 3 символов
 string[] NewStrArray = MakeArray(StrArray);
